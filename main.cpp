82a9987165cf24d225fac1f4b39bbdc2af7fff75
Dee Kay <dee@dee-Inspiron-N5110.(none)>
2013-03-20 02:34:54 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+class demo
+{
+    protected:
+    int x;
+    public:
+    void getdata()
+    {
+        cout<<"enter the value of x =";
+        cin>>x;
+    }
+    void displaydata()
+    {
+        cout<<"x = "<<x<<endl;
+    }
+};
+class demo1
+{
+    protected:
+    int y;
+    public:
+    void recievedata()
+    {
+        cout<<"enter the value of y =";
+        cin>>y;
+    }
+    void putdata()
+    {
+        cout<<"y ="<<y<<endl;
+    }
+};
+class demo2:public demo1,public demo
+{
+    private:
+    int z;
+    public:
+    void adddata()
+    {
+        z=x+y;
+    }
+    void showdata()
+    {
+        cout<<"value of z ="<<z<<endl;
+    }
+};
+int main()
+{
+    demo2 d;
+d.getdata();
+d.displaydata();
+d.recievedata();
+d.putdata();
+d.adddata();
+d.showdata();
+return 0;
+}
